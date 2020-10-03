ee6e20ddd51cf380da503e7df01ec3ff74c5ea96
SquidHat14 <58124363+SquidHat14@users.noreply.github.com>
2020-10-03 09:36:53 -0400

    messed up commits

--- src/app/spotify/spotify_auth.ts
+++ src/app/spotify/spotify_auth.ts
@@ -1,4 +1,4 @@
 export const spotify_auth = {
-    clientID : 'bb334333021d47ad99e8d7fa9c3465c0',
-    clientSecret : '73816233224a40e6b9e1e6a087991dfb',
+    clientID : 'CLIENT ID HERE',
+    clientSecret : 'CLIENT SECRET HERE',
 }
